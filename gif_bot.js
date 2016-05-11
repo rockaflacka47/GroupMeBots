f4f18c4678ffa72453b127a977d18186c70e1a27
akd40 <akd40@pitt.edu>
2016-05-10 20:06:23 -0400

    Test depolyment 1 of gifBot

--- gif_bot.js
+++ gif_bot.js
@@ -1,38 +1,24 @@
 var HTTPS = require('https');
 //var gif = require('./gif');
-var request = require('request');
-
+//var request = require('request');
 var botID = process.env.GIF_BOT_ID;
 
-function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/gif*/;
+function respond(request) {
+  var search = request.replace("/gif ", "");
+  search = request.replace("/gif", "");
+  search = search.replace(" ","");
 
-  if(request.text && botRegex.test(request.text)) {
-    var search = request.text.replace("/gif ", "");
-    search = request.text.replace("/gif", "");
-    search = search.replace(" ","");
-    var sanityCheck = /^[a-zA-Z0-9 ]+$/;
-    console.log(!(sanityCheck.test(search)));
-    if(!(sanityCheck.test(search))) {
-      postMessage("NO, you're trying to hack me :(");
-    } else {
-      var theURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC&limit=5";
-      var temp;
-      getGif(theURL, function(result){
-        console.log("echo " + result);
-        postMessage(result);
-      });
-    }
-
-    this.res.writeHead(200);
-    //postMessage(theURL);
-    this.res.end();
-
+  var sanityCheck = /^[a-zA-Z0-9 ]+$/;
+  console.log(!(sanityCheck.test(search)));
+  if(!(sanityCheck.test(search))) {
+    postMessage("NO, you're trying to hack me :(");
   } else {
-    //console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+    var theURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC&limit=5";
+    var temp;
+    getGif(theURL, function(result){
+      console.log("echo " + result);
+      postMessage(result);
+    });
   }
 }
 
@@ -49,7 +35,6 @@ function getGif(url, callback) {
       } catch (ex) {
         console.log("No such element");
       }
-
     }
   });
 }
